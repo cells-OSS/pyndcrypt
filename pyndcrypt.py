3f02933682e9e938284f0831f8f3a507a9b7a302
cells-OSS <open-source-version@proton.me>
2025-10-18 23:17:39 +0300

    BugFix
    Fixed auto update settings not working, added custom location support for auto update.

--- pyndcrypt.py
+++ pyndcrypt.py
@@ -275,20 +275,22 @@ if chooseOption == "3":
                 os.execv(sys.executable, [sys.executable] + sys.argv)
 
             if aUpdateOption == "1":
-                with open("auto_update.conf", "wb") as auto_update_configFile:
+                config_path = os.path.join(config_dir, "auto_update.conf")
+
+                with open(config_path, "wb") as auto_update_configFile:
                     auto_update_configFile.write("True".encode())
 
-                    print("Changes saved successfully!")
-                    input("Press any key to restart...")
-                    os.execv(sys.executable, [sys.executable] + sys.argv)
+                print("Changes saved successfully!")
+                input("Press any key to restart...")
+                os.execv(sys.executable, [sys.executable] + sys.argv)
 
             if aUpdateOption == "2":
-                with open("auto_update.conf", "wb") as auto_update_configFile:
+                with open(config_path, "wb") as auto_update_configFile:
                     auto_update_configFile.write("False".encode())
 
-                    print("Changes saved successfully!")
-                    input("Press any key to restart...")
-                    os.execv(sys.executable, [sys.executable] + sys.argv)
+                print("Changes saved successfully!")
+                input("Press any key to restart...")
+                os.execv(sys.executable, [sys.executable] + sys.argv)
             
             else:
                 print("Invalid option.")
